fa1a48e5518e1fb83a6880a248e8e96855bf6a01
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-03-21 13:03:32 +0300

    Создал requirements.txt и докер файл

--- wallet_app/wallet/models.py
+++ wallet_app/wallet/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Wallet(models.Model):
+    id = models.CharField(max_length=36, primary_key=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
